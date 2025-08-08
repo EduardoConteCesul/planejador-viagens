d03b16e1756175fbecd10d7ba517b5f266cd637c
EduardoConteCesul <eduardo.2024014337@alunocesul.com>
2025-08-07 22:31:52 -0300

    Configuration TripController

--- src/main/java/com/conteduu/planejadorviagens/MainApp.java
+++ src/main/java/com/conteduu/planejadorviagens/MainApp.java
@@ -1,13 +1,21 @@
 package com.conteduu.planejadorviagens;
 
-/**
- * Hello world!
- *
- */
-public class MainApp
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Override
+    public void start(Stage stage) throws Exception {
+        var loader = new FXMLLoader(getClass().getResource("/com.conteduu.planejadorviagens/trip_view.fxml"));
+
+        stage.setTitle("Trip Planner - MVC");
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
     }
+
+
+    public static void main( String[] args ){launch(args);}
 }
--- src/main/java/com/conteduu/planejadorviagens/controller/TripController.java
+++ src/main/java/com/conteduu/planejadorviagens/controller/TripController.java
@@ -1,9 +1,13 @@
 package com.conteduu.planejadorviagens.controller;
 
 import com.conteduu.planejadorviagens.model.Viagem;
+import com.conteduu.planejadorviagens.service.PlanejamentoService;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javax.swing.table.TableColumn;
+
+import java.time.format.DateTimeFormatter;
 
 public class TripController {
     // Elements FXML
@@ -20,13 +24,64 @@ public class TripController {
     @FXML
     private TableView<Viagem> tripTable;
     @FXML
-    private TableColumn colCity;
+    private TableColumn<Viagem, String> colCity;
     @FXML
-    private TableColumn colStart;
+    private TableColumn<Viagem, String> colStart;
     @FXML
-    private TableColumn colEnd;
+    private TableColumn<Viagem, String> colEnd;
     @FXML
-    private TableColumn colCost;
+    private TableColumn<Viagem, String> colCost;
 
+    private final PlanejamentoService planejamentoService = new PlanejamentoService();
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    @FXML
+    private void initialized() {
+        // Define como cada coluna extrai informação da entidade
+
+        // Se só devolver uma String simples, o JFX não consegue saber se o valor mudou depois.
+        // Por isso se devolver uma SimpleStringProperty ele pode observar aquela propriedade
+        // e atualizar a interface automaticamente.
+        colCity.setCellValueFactory( colCity ->
+                new SimpleStringProperty(colCity.getValue().getDestino())
+        );
+
+        colStart.setCellValueFactory( start ->
+                        new SimpleStringProperty(start.getValue().getDataInicio().format(formatter))
+        );
+
+        colEnd.setCellValueFactory(end ->
+                new SimpleStringProperty(end.getValue().getDataFim().format(formatter))
+        );
+
+        colCost.setCellValueFactory(cost ->
+                new SimpleStringProperty(cost.getValue().getDataFim().format(formatter))
+        );
+
+        // Preencher a tabela com dados já gravados.
+
+        tripTable.setItems(FXCollections.observableArrayList(planejamentoService.listAll()));
+
+    }
+
+    @FXML
+    public void addTrip() {
+        try {
+            double cost = Double.parseDouble(
+                    costField.getText().replace(",", ".")
+            );
+        } catch (Exception e) {
+            mostrarErro(e.getMessage());
+        }
+    }
+
+    private void mostrarErro(String errorMessage) {
+        new Alert(Alert.AlertType.ERROR, errorMessage).showAndWait();
+    }
+
+    private void updateCost() {
+        costField.setText("Total: R$ " + String.format("%.2f", planejamentoService.totalExpense()));
+    }
 
 }
